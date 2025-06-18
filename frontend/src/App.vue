5bbf213e3488cfbaed5d2de94cf58c5cee4f80bb
Andrew Clinton <ddrew.clinton@gmail.com>
2025-06-17 23:01:27 -0400

    move base wrapper styling to parent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <RouterView />
+  </div>
 </template>
