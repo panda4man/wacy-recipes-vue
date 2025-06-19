2d132500726010ac6c243c021cd952b7fa0056bc
Andrew Clinton <ddrew.clinton@gmail.com>
2025-06-18 22:02:07 -0400

    more clean up

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -14,7 +14,7 @@ class DemoSeeder extends Seeder
     {
         \App\Models\Recipe::factory(200)
             ->create()
-            ->each(function ($recipe) {
+            ->each(function (\App\Models\Recipe $recipe) {
                 $steps = collect(range(1, fake()->numberBetween(3, 10)))
                     ->map(function ($i) {
                         return \App\Models\Step::factory()->make(['order' => $i]);
